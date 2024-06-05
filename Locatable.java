b07f88c9e95e06dd4cfca4b02c079242d58aa0e3
pb <87837637+perneet-bhathal@users.noreply.github.com>
2024-06-05 10:16:12 -0700

    Update Locatable.java

--- Locatable.java
+++ Locatable.java
@@ -1,9 +1,11 @@
 public interface Locatable{
   public void setPos(int x, int y);
 
+  // mutator methods
   public void setX(int x);
   public void setY(int y);
 
+  // accesor methods
   public int getX();
   public int getY();
 }
